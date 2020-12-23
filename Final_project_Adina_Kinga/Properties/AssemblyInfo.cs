2cece0dcd0b60ca553275bcbe62fff78c2eea312
KingaStuchirenea <72432661+KingaStuchirenea@users.noreply.github.com>
2020-12-23 15:05:28 +0200

    Update nuget packages
    Update nuget packages

---
+++ Final_project_Adina_Kinga/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Final_project_Adina_Kinga")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Final_project_Adina_Kinga")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3975c40-8b58-4650-9adc-c96524c99553")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
